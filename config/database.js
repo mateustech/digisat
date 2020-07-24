8bdedd8df6533dcd836656829ba5289f359fb929
Mateus Fernandes Costa <mateustech686@gmail.com>
2020-07-23 23:47:01 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -29,14 +29,14 @@ module.exports = {
   | npm i --save sqlite3
   |
   */
-  sqlite: {
-    client: 'sqlite3',
-    connection: {
-      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
-    },
-    useNullAsDefault: true,
-    debug: Env.get('DB_DEBUG', false)
-  },
+  // sqlite: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+  //   },
+  //   useNullAsDefault: true,
+  //   debug: Env.get('DB_DEBUG', false)
+  // },
 
   /*
   |--------------------------------------------------------------------------
@@ -48,17 +48,17 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  mysql: {
-    client: 'mysql',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    },
-    debug: Env.get('DB_DEBUG', false)
-  },
+  // mysql: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: Env.get('DB_HOST', 'localhost'),
+  //     port: Env.get('DB_PORT', ''),
+  //     user: Env.get('DB_USER', 'root'),
+  //     password: Env.get('DB_PASSWORD', ''),
+  //     database: Env.get('DB_DATABASE', 'adonis')
+  //   },
+  //   debug: Env.get('DB_DEBUG', false)
+  // },
 
   /*
   |--------------------------------------------------------------------------
@@ -70,6 +70,7 @@ module.exports = {
   | npm i --save pg
   |
   */
+
   pg: {
     client: 'pg',
     connection: {
