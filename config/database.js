afa8679d2eaefe7c8a307abfb2145a4313701cdd
Mateus Fernandes Costa <mateustech686@gmail.com>
2020-07-23 23:56:44 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use("Env");
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use("Helpers");
+
+const Url = require("url-parse");
+const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
   /*
@@ -16,7 +19,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'pg'),
+  connection: Env.get("DB_CONNECTION", "pg"),
 
   /*
   |--------------------------------------------------------------------------
@@ -72,14 +75,14 @@ module.exports = {
   */
 
   pg: {
-    client: 'pg',
+    client: "pg",
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get("DB_HOST", DATABASE_URL.hostname),
+      port: Env.get("DB_PORT", DATABASE_URL.port),
+      user: Env.get("DB_USER", DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     },
-    debug: Env.get('DB_DEBUG', false)
-  }
-}
+    debug: Env.get("DB_DEBUG", false),
+  },
+};
