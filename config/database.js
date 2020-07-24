38a37b79a1bd4634cd800f5323f87e6e22775bcd
Mateus Fernandes Costa <mateustech686@gmail.com>
2020-07-24 00:20:50 -0300

    make it better

--- config/database.js
+++ config/database.js
@@ -1,13 +1,10 @@
-"use strict";
+'use strict'
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use("Env");
+const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use("Helpers");
-
-const Url = require("url-parse");
-const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
+const Helpers = use('Helpers')
 
 module.exports = {
   /*
@@ -19,7 +16,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get("DB_CONNECTION", "pg"),
+  connection: Env.get('DB_CONNECTION', 'sqlite'),
 
   /*
   |--------------------------------------------------------------------------
@@ -32,14 +29,14 @@ module.exports = {
   | npm i --save sqlite3
   |
   */
-  // sqlite: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
-  //   },
-  //   useNullAsDefault: true,
-  //   debug: Env.get('DB_DEBUG', false)
-  // },
+  sqlite: {
+    client: 'sqlite3',
+    connection: {
+      filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
+    },
+    useNullAsDefault: true,
+    debug: Env.get('DB_DEBUG', false)
+  },
 
   /*
   |--------------------------------------------------------------------------
@@ -51,17 +48,17 @@ module.exports = {
   | npm i --save mysql
   |
   */
-  // mysql: {
-  //   client: 'mysql',
-  //   connection: {
-  //     host: Env.get('DB_HOST', 'localhost'),
-  //     port: Env.get('DB_PORT', ''),
-  //     user: Env.get('DB_USER', 'root'),
-  //     password: Env.get('DB_PASSWORD', ''),
-  //     database: Env.get('DB_DATABASE', 'adonis')
-  //   },
-  //   debug: Env.get('DB_DEBUG', false)
-  // },
+  mysql: {
+    client: 'mysql',
+    connection: {
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', '')
+    },
+    debug: Env.get('DB_DEBUG', false)
+  },
 
   /*
   |--------------------------------------------------------------------------
@@ -73,16 +70,15 @@ module.exports = {
   | npm i --save pg
   |
   */
-
   pg: {
-    client: "pg",
+    client: 'pg',
     connection: {
-      host: Env.get("DB_HOST", DATABASE_URL.hostname),
-      port: Env.get("DB_PORT", DATABASE_URL.port),
-      user: Env.get("DB_USER", DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+      host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT', ''),
+      user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD', ''),
+      database: Env.get('DB_DATABASE', 'adonis')
     },
-    debug: Env.get("DB_DEBUG", false),
-  },
-};
+    debug: Env.get('DB_DEBUG', false)
+  }
+}
