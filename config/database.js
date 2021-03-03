b9456e60fc67b262838a0a7c4553665515fb11ed
mateustech <mateustech686@gmail.com>
2021-03-03 18:41:16 -0300

    Purge cache

--- config/database.js
+++ config/database.js
@@ -10,17 +10,17 @@ const Url = require("url-parse");
 const DATABASE_URL = new Url(Env.get("DATABASE_URL"));
 
 module.exports = {
-  connection: Env.get(DATABASE_URL),
+  connection: Env.get(DATABASE_URL.DB_CONNECTION, "pg"),
 
   //comentario
   pg: {
     client: "pg",
     connection: {
-      host: Env.get("DB_HOST"),
-      port: Env.get("DB_PORT"),
-      user: Env.get("DB_USER"),
-      password: Env.get("DB_PASSWORD"),
-      database: Env.get("DB_DATABASE"),
+      host: Env.get("DB_HOST", DATABASE_URL.hostname),
+      port: Env.get("DB_PORT", DATABASE_URL.port),
+      user: Env.get("DB_USER", DATABASE_URL.username),
+      password: Env.get("DB_PASSWORD", DATABASE_URL.password),
+      database: Env.get("DB_DATABASE", DATABASE_URL.DB_DATABASE),
     },
     debug: Env.get("DB_DEBUG", false),
   },
