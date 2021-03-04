750b300cdc8d8d0dd5d95e5cc8d2824d243ea9a2
mateustech <mateustech686@gmail.com>
2021-03-04 11:31:06 -0300

    asdas

--- config/database.js
+++ config/database.js
@@ -16,11 +16,11 @@ module.exports = {
   pg: {
     client: "pg",
     connection: {
-      host: Env.get("DB_HOST", POSTGRES_DBHOST),
-      port: Env.get("DB_PORT", POSTGRES_DBPORT),
-      user: Env.get("DB_USER", POSTGRES_DBUSER),
-      password: Env.get("DB_PASSWORD", POSTGRES_DBPASS),
-      database: Env.get("DB_DATABASE", POSTGRES_DBNAME),
+      host: Env.get("DB_HOST", "POSTGRES_DBHOST"),
+      port: Env.get("DB_PORT", "POSTGRES_DBPORT"),
+      user: Env.get("DB_USER", "POSTGRES_DBUSER"),
+      password: Env.get("DB_PASSWORD", "POSTGRES_DBPASS"),
+      database: Env.get("DB_DATABASE", "POSTGRES_DBNAME"),
     },
     debug: Env.get("DB_DEBUG", false),
   },
